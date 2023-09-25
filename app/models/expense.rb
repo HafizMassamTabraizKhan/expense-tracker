395e40b11d44ebcbe64d2081b557cb8b2c3eb5b0
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-09-26 00:39:27 +0500

    Add associations to category model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,4 @@
 class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_and_belongs_to_many :category dependent: :destroy
+  has_and_belongs_to_many :categories, dependent: :destroy
 end
