e549316a2a703c2d866f6340a2347e60c659b653
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-09-26 00:49:34 +0500

    Modify associations in expense and category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-  has_and_belongs_to_many: expenses, dependent: :destroy
+  has_and_belongs_to_many: expenses, join_table: 'expense_categories', dependent: :destroy
 end
