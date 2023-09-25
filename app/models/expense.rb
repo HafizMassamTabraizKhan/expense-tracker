c02281e9794504ee334867bb768dc3c2a1d03e4b
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-09-26 01:07:50 +0500

    Run migrations

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,5 @@
 class Expense < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   has_and_belongs_to_many :categories, join_table: 'expense_categories', dependent: :destroy
 
   validates :author_id, presence: true
