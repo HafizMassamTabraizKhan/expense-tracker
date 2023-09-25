ce52a3afcf507e4ef44667cce41e35d39b13a45e
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-09-26 00:54:24 +0500

    Add associations in expense_category model

--- app/models/expense_category.rb
+++ app/models/expense_category.rb
@@ -1,4 +1,4 @@
 class ExpenseCategory < ApplicationRecord
-  belongs_to :expense
-  belongs_to :category
+  belongs_to :expense, class_name: 'Expense', foreign_key: 'expense_id'
+  belongs_to :category, class_name: 'Category', foreign_key: 'category_id'
 end
